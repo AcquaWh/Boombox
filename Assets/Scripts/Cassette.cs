4a461e89a001abc114b1c7c507aa24e7763497c3
ChavezDavid <david_12-95@hotmail.com>
2019-09-05 09:13:26 -0700

    Reproduccion de audio
    Con estos scripts se reproduce el audio cuando el cassette toca el reproductor

---
+++ Assets/Scripts/Cassette.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cassette : MonoBehaviour
+{
+    public string name;
+    public int speed;
+    public int driveForce;
+    public int jumpForce;
+    public GameObject player;
+    Renderer renderer;
+    AudioSource cancion;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cancion = GetComponent<AudioSource>();
+        renderer = GetComponent<Renderer>();
+
+        transform.position = player.transform.position - Vector3.left * 10f;
+
+        Debug.Log("I am alive and my name is " + name + ", Rigidbody: " + rb + ", cancion: " + cancion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * distance);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 force = transform.forward * driveForce * Input.GetAxis("Vertical");
+        rb.AddForce(force);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 150, 100), "Game Over");
+        if (GUI.Button(new Rect(10, 10, 150, 100), "Reproducir"))
+        {
+            if (!cancion.isPlaying)
+            {
+                cancion.Play();
+            }
+            /*Vector3 force = transform.up * jumpForce;
+            rb.AddForce(force);*/
+        }
+        if (GUI.Button(new Rect(160, 10, 150, 100), "Pausar"))
+        {
+            cancion.Pause();
+            /*Vector3 force = transform.up * jumpForce;
+            rb.AddForce(force);*/
+        }
+        if (GUI.Button(new Rect(310, 10, 150, 100), "Detener"))
+        {
+            cancion.Stop();
+            /*Vector3 force = transform.up * jumpForce;
+            rb.AddForce(force);*/
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    AudioSource cancion;
+
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        Debug.Log("I am an enemy, Renderer: " + renderer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            StartCoroutine("Fade");
+        }
+    }
+
+    void OnCollisionEnter(Collision otherObj)
+    {
+        if (otherObj.gameObject.tag == "Player")
+        {
+            cancion = otherObj.gameObject.GetComponent<AudioSource>();
+            Debug.Log("I am an enemy, Cancion: " + cancion.name);
+            if (!cancion.isPlaying)
+            {
+                cancion.Play();
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision otherObj)
+    {
+        if (otherObj.gameObject.tag == "Player")
+        {
+            cancion = otherObj.gameObject.GetComponent<AudioSource>();
+            Debug.Log("I am an enemy, Cancion: " + cancion.name);
+            cancion.Pause();
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.1f)
+        {
+            Color c = renderer.material.color;
+            c.a = ft;
+            renderer.material.color = c;
+            yield return null;
+        }
+    }
+}
